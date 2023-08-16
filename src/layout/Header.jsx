14339bd20c3637282dea8c40dfe17f9ad8156869
Aeron01 <one.soulreaper@gmail.com>
2023-08-16 17:57:03 +0200

    working on sass :construction:

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { NavLink} from 'react-router-dom'
+import { NavLink} from 'react-router-dom';
+import './index.css';
 
 function Header() {
     const logo = '/assets/logo-kasa.svg'
